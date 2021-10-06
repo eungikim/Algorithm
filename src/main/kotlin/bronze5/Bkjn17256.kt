f94475810e4a9e7ae893a8c9838a3d6aea585483
eungikim <eungikim.me@gmail.com>
2021-10-06 17:01:06 +0900

    Solve https://www.acmicpc.net/problem/17256

---
+++ src/main/kotlin/bronze5/Bkjn17256.kt
@@ -0,0 +1,29 @@
+package bronze5
+
+// https://www.acmicpc.net/problem/17256
+fun main() {
+    val input1 = readLine()?.split(" ")?.map { it.toInt() }!!
+    val input2 = readLine()?.split(" ")?.map { it.toInt() }!!
+    val cakeA = Cake(input1)
+    val cakeC = Cake(input2)
+
+    val cakeB = cakeA reversCake cakeC
+    println(cakeB)
+}
+
+data class Cake(val x: Int, val y: Int, val z: Int) {
+
+    constructor(array: List<Int>): this(array[0], array[1], array[2])
+    // this 🍰 other = return
+    infix fun cake(other: Cake): Cake {
+        return Cake(z + other.x, y * other.y, x + other.z)
+    }
+    // this 🍰 return = result
+    infix fun reversCake(result: Cake): Cake {
+        return Cake(result.x - z, result.y / y, result.z - x)
+    }
+
+    override fun toString(): String {
+        return "$x $y $z"
+    }
+}
